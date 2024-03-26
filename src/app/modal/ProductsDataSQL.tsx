38361b875541800543c549f9edd43be6e7a88ef3
BaselessFabric <87932619+BaselessFabric@users.noreply.github.com>
2024-03-26 19:13:10 +0000

    changed hardcoded api address to env

--- src/app/modal/ProductsDataSQL.tsx
+++ src/app/modal/ProductsDataSQL.tsx
@@ -1,7 +1,7 @@
 import ProductClass from "./ProductClass";
 
 export function fetchProducts(): Promise<ProductClass[]> {
-  return fetch("http://localhost:3001/api/products")
+  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`)
     .then((response) => response.json())
     .then((data) =>
       data.map(
@@ -18,7 +18,7 @@ export function fetchProducts(): Promise<ProductClass[]> {
 }
 
 export function fetchCategories(): Promise<string[]> {
-  return fetch("http://localhost:3001/api/categories")
+  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`)
     .then((response) => response.json())
     .then((data) => data.map((category: any) => category.category_name));
 }
